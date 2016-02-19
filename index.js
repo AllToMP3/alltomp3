fd7eb62fbb36db9aae4fbf38f67acca0973ef11e
Basile Bruneau <basilebruneau@gmail.com>
2016-02-19 17:43:57 +0100

    Create empty node project

---
+++ index.js
@@ -0,0 +1,13 @@
+var youtubedl = require('youtube-dl');
+
+/**
+ * Download a single video with youtube-dl
+ * @param url
+ * @param file
+ */
+function downloadWithYoutubeDl(url, file) {
+
+}
+
+
+module.exports = {};
