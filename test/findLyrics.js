7c073d9b55c586d8682eb4e05b486deef939093d
Basile Bruneau <basilebruneau@gmail.com>
2016-04-19 10:21:02 +0200

    Update test name

--- test/findLyrics.js
+++ test/findLyrics.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-describe('Array', function() {
+describe('findLyrics', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
